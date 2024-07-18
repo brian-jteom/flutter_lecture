17b59a93d94d97636b1c18fb698ce4a17adf1dae
Eom Juntae <junbrian9815@gmail.com>
2024-07-18 11:54:27 +0900

    json data 조회

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'youtube.dart';
+import 'jsonload_future.dart';
 
 void main() {
   runApp(const MyApp());
---
+++ lib/shared_preferences.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  static Future loadJson() async {
+    final String response = await rootBundle.loadString("lib/users.json");
+    final data = await json.decode(response);
+    return data['users'];
+  }
+
+  Future userList = loadJson();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("test title"),
+        ),
+        body: Container(
+          child: FutureBuilder(
+            future: userList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                            "${snapshot.data[index]["id"]}: ${snapshot.data[index]["username"]}"
+                        ),
+                      );
+                    }
+                );
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text("Error"),
+                );
+              } else {
+                return const Center(
+                    child: CircularProgressIndicator(
+                        strokeWidth: 2,
+                    )
+                );
+              }
+            },
+          ),
+        )
+    );
+  }
+}
